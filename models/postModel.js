0cdb91af9e3513c0136560f196a3ca42230557fd
bill60800 <rubxx40734@gmail.com>
2022-05-03 10:51:19 +0800

    edit createAT

--- models/postModel.js
+++ models/postModel.js
@@ -13,7 +13,6 @@ const postSchema = new mongoose.Schema(
           createdAt: {
             type: Date,
             default: Date.now,
-            select: false
           },
           user: {
               type: mongoose.Schema.ObjectId,
